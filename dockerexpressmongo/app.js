bd4a93f9bff9f3e72c747e1f336e87452f001c02
Aleksander Didriksen <aleksadi@stud.ntnu.no>
2019-10-04 20:00:55 +0200

    Hotfix of cors function

--- dockerexpressmongo/app.js
+++ dockerexpressmongo/app.js
@@ -9,7 +9,7 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(cors({credentials: true, origin: true}));
+app.use(cors());
 app.options('*', cors());
 
 
