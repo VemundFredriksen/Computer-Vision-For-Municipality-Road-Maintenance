34165c6395d46d756c72bb143362030b880bf301
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2019-10-09 16:22:01 +0200

    Train and log (#55)
    * initial preprocces validation added to training.py
    
    * minor update
    
    * removed darknet as folder
    
    * added darknet as submodule
    
    * Added darknet as files rather than submodule because the need to alter the source code
    
    * trying to push darknet files
    
    * deleted submodule yet again...
    
    * did i finally get to push the darknet files??
    
    * cleaned repo
    
    * Penetrated the darknet pipeline, prediction with loaded net is now possible and ported into python
    
    * almost done wrapping up validation, will continue in next sprint

---
+++ cvmodule/utilities/c_bindings.py
@@ -0,0 +1,38 @@
+from ctypes import *
+
+class BOX(Structure):
+    _fields_ = [("x", c_float),
+                ("y", c_float),
+                ("w", c_float),
+                ("h", c_float)]
+
+class DETECTION(Structure):
+    _fields_ = [("bbox", BOX),
+                ("classes", c_int),
+                ("prob", POINTER(c_float)),
+                ("mask", POINTER(c_float)),
+                ("objectness", c_float),
+                ("sort_class", c_int)]
+
+lib = CDLL("./../core/darknet/libdarknet.so", RTLD_GLOBAL)
+
+# ======== Loads the neural net into GPU Memory ======== #
+load_net = lib.load_network
+load_net.argtypes = [c_char_p, c_char_p, c_int]
+load_net.restype = c_void_p
+
+# ======== Fetches the GPU index ======== #
+fetch_gpus = lib.fetch_gpus
+fetch_gpus.argtypes = []
+fetch_gpus.restype = POINTER(c_int)
+
+# ======== Envokes the training function in darknet core code ======== #
+train = lib.custom_train
+train.argtypes = [c_char_p, c_char_p, c_int, POINTER(c_int), c_int, c_int, c_int, c_int]
+train.restype = c_float
+
+# ======== Makes a detection of given image ======== #
+predict = lib.predict
+predict.argtypes = [c_char_p, c_char_p, c_int, c_char_p, c_float, c_float, c_char_p, c_int]
+predict.restype = POINTER(DETECTION)
+
---
+++ cvmodule/utilities/train.py
@@ -0,0 +1,116 @@
+# Imports
+
+import argparse
+from os import path
+import c_bindings as cb
+import predict
+import validate
+
+
+#Validate and store
+def fetch_val_images(file):
+    f = open(file)
+    lines = f.readlines()
+    f.close()
+    ims = []
+    for l in lines:
+        ims.append(l.strip())
+    return ims
+
+#Load Net
+
+def load_network(cfg, weights):
+    return cb.load_net(cfg.encode("UTF-8"), weights.encode("UTF-8"), 0)
+
+# PreProcessing
+
+def number_of_datafiles(cfg_file):
+    c = open(cfg_file, "r")
+    cfg_lines = c.readlines()
+    c.close()
+    
+    file = cfg_lines[1].split('=')[1].replace("\n", "").strip()
+
+    f = open(file, "r")
+    lines = f.readlines()
+    f.close()
+    return len(lines)
+
+def file_exists(file):
+    return path.exists(file)
+
+def validate_data_file(file, ignore_missing_validation=False):
+    f = open(file, "r")
+    lines = f.readlines()
+    f.close()
+    if(not str(lines[0].split('=')[1].replace("\n", "").strip()).isdigit()):
+        print("Data file is corrupted, please specify classes at line 1 'classes = {int}'")
+        return False
+    if(not file_exists(lines[1].split('=')[1].replace("\n", "").strip())):
+       print("Path to training data is invalid, check line 2 in data file")
+       return False
+    if(not file_exists(lines[3].split('=')[1].replace("\n", "").strip())):
+        print("Path to names file is invalid, check line 4 in data file")
+        return False
+
+    names = open(lines[3].split('=')[1].replace("\n", "").strip())
+    n_names = len(names.readlines())
+    names.close()
+    if(not n_names == int(lines[0].split('=')[1].replace("\n", "").strip())):
+        print("Number of names in names file does not correspond to specified number of classes in .data file")
+        return False
+    if(not file_exists(lines[2].split('=')[1].replace("\n", "").strip())):
+        if(not ignore_missing_validation):
+            inp = input("Path to validation data is invalid, do you want to continue? [y/n]\n")
+            while(not inp.lower() == 'y' and not inp.lower() == 'n'):
+               inp = input("Path to validation data is invalid, do you want to continue? [y/n]\n")
+            if(inp.lower() == 'n'):
+               return False
+
+    return True
+
+def pre_process(cfg, weights, train_txt, ignore_validation_txt):
+
+    '''Check Files existence '''
+    if(not file_exists(cfg)):
+        print("'{}' not found".format(cfg))
+        return False
+    elif(not file_exists(weights)):
+        print("'{}' not found".format(weights))
+        return False
+    elif(not file_exists(train_txt)):
+        print("'{}' not found".format(train_txt))
+        return False
+
+    return validate_data_file(train_txt, ignore_validation_txt)
+
+# Main
+
+def main(cfg, weights, train_txt, ignore_validation_txt):
+    if(not pre_process(cfg, weights, train_txt, ignore_validation_txt)):
+        print("Training stopped premature")
+        return
+    net = load_network(cfg, weights)
+    n_data = number_of_datafiles(train_txt)
+
+    max_epochs = 25
+    
+    val_ims = fetch_val_images("./val.txt")
+    
+    for i in range(1, max_epochs + 1):
+        loss = cb.train(train_txt.encode("UTF-8"), cfg.encode("UTF-8"), net, cb.fetch_gpus(), 1, 0, 300, i);
+        print("Epoch {} done! Average loss: {}".format(i, loss))
+        print("Validating loss...")
+        validate.validate_and_log(cfg, net, val_ims, loss)
+   
+
+if(__name__ == "__main__"):
+    parser = argparse.ArgumentParser(description='Train specified network on specified data')
+
+    parser.add_argument('-c','--cfg', help='The network configuration file', required=True)
+    parser.add_argument('-w','--weights', help='The network weights', required=True)
+    parser.add_argument('-t','--train', help='obj.data file that configures the training', required=True)
+    parser.add_argument('-v','--igValid', help='Flag to indicate if missing validation file is OK', action="store_true")
+    args = vars(parser.parse_args())
+    
+    main(args["cfg"], args["weights"], args["train"], args["igValid"])
