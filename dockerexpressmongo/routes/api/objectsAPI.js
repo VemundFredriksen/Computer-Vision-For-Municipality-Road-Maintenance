aed0e547d7728aff6225442dbb1c13d64595d593
Aleksander Didriksen <aleksadi@stud.ntnu.no>
2019-11-09 17:57:03 +0100

    Change query typo to body

--- dockerexpressmongo/routes/api/objectsAPI.js
+++ dockerexpressmongo/routes/api/objectsAPI.js
@@ -115,7 +115,7 @@ router.get("/get-object-by-id", (req, res, next) => {
 
 // Same as above, but queries for multiple ids using the $in operator
 router.get("/get-objects-by-ids", (req, res, next) => {
-  detectedObjectDB.find({ "_id": { "$in": req.query.ids } }, function (err, objects) {
+  detectedObjectDB.find({ "_id": { "$in": req.body.ids } }, function (err, objects) {
     if (err) {
       return res.status(400).json({ msg: "Could not find the Objects"});      
     }
