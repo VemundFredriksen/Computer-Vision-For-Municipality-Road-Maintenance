89d1bc68d5ed04f59e583b1dfbbf6cdaf8bdc773
Ingrid <ingrid@Ingrids-MacBook-Pro.local>
2019-11-09 15:50:56 +0100

    change marker color when in WO list

--- webapp/src/components/MapComponent.js
+++ webapp/src/components/MapComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import { Icon } from 'leaflet';
 import PropTypes from 'prop-types';
+import isSubset from 'is-subset';
 
 import './MapComponent.css';
 
-const redIcon = new L.Icon({
+const redIcon = new Icon({
   iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
   iconSize: [25, 41],
@@ -13,7 +15,7 @@ const redIcon = new L.Icon({
   shadowSize: [41, 41],
 });
 
-const blueIcon = new L.Icon({
+const blueIcon = new Icon({
   iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
   iconSize: [25, 41],
@@ -22,27 +24,45 @@ const blueIcon = new L.Icon({
   shadowSize: [41, 41],
 });
 
+const greenIcon = new Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
 
-const MapComponent = ({ objects, onMarkerClick }) => {
-  return (
-    <Map className="map" center={[63.42, 10.39]} zoom={12}>
-      <TileLayer
-        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+function findCorrectMarker(object, workOrders) {
+  const obj = workOrders.filter((item) => isSubset(object, item));
+  if (obj.length !== 0) {
+    return greenIcon;
+  }
+  if (object.approved) {
+    return blueIcon;
+  }
+  return redIcon;
+}
+
+
+const MapComponent = ({ objects, onMarkerClick, workOrders }) => (
+  <Map className="map" center={[63.42, 10.39]} zoom={12}>
+    <TileLayer
+      attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    {objects.map((object) => (
+      <Marker
+        key={object._id}
+        position={object.coordinates}
+        icon={findCorrectMarker(object, workOrders)}
+        onClick={() => {
+          onMarkerClick(object._id);
+        }}
       />
-      {objects.map((object) => (
-        <Marker
-          key={object._id}
-          position={object.coordinates}
-          icon={object.approved ? blueIcon : redIcon}
-          onClick={() => {
-            onMarkerClick(object._id);
-          }}
-        />
-      ))}
-    </Map>
-  );
-};
+    ))}
+  </Map>
+);
 
 MapComponent.propTypes = {
   objects: PropTypes.arrayOf(
@@ -55,11 +75,13 @@ MapComponent.propTypes = {
     }),
   ),
   onMarkerClick: PropTypes.func,
+  workOrders: PropTypes.arrayOf(PropTypes.object),
 };
 
 MapComponent.defaultProps = {
   objects: [],
   onMarkerClick: null,
+  workOrders: [],
 };
 
 export default MapComponent;
