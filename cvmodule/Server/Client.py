cc4c89b02690b5195224a5bbfbec33758552a848
formalatist <formalatist@gmail.com>
2019-11-18 02:16:50 +0100

    Console should look nicer during analysis now

--- cvmodule/Server/Client.py
+++ cvmodule/Server/Client.py
@@ -7,9 +7,6 @@ def upload_image(path_to_image):
 		files = files)
 
 def upload_data(path_to_data):
-	f = open(path_to_data, "r+")
-	print(f.read())
-	f.close()
 	JSON = json.loads(open(path_to_data, "r+").read())
 	requests.post("http://api.dewp.eu.org:4000/insert-objectdata", 
 		json=JSON)
